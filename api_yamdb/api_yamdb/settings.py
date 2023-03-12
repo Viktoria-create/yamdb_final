b7ba85212b28f0cf478ee48fb8c0d7140e2b4932
Viktoria-create <viktoriakoshelnik@yandex.ru>
2023-03-12 20:32:09 +0300

    92

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -107,10 +107,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/app/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/app/media/'
 
 AUTH_USER_MODEL = 'reviews.User'
 
