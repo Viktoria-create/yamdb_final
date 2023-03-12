1d0b281245237ee531dc23a0dd8cbd9ab848acc7
Viktoria-create <viktoriakoshelnik@yandex.ru>
2023-03-12 16:51:17 +0300

    89

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
+    path('admin/', include('myapp.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
